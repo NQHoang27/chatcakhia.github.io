160a9e3ef8c4f3d0d9cc5be00f6a91c632e43201
Hoangnq <hoangnq.jvb@gmail.com>
2019-09-26 17:17:35 +0700

    fix datetime

--- js/realtime.js
+++ js/realtime.js
@@ -4,8 +4,11 @@ app.controller('chatCtrl', ['$scope', '$firebase', function($scope, $firebase) {
 	var image = "https://ptetutorials.com/images/user-profile.png";
 	$scope.name = name;
 	$scope.chatMessage = "";
-	if (name && $scope.chatMessage != null) {
-		
+	if (name != "") {
+		var date = new Date();
+		var strDate = date.getDate() + "/" + (date.getMonth()+1) + "/" + date.getFullYear();
+		var hours = date.getHours();
+		var min = date.getMinutes();
 		var ref = new Firebase("https://chatcakhia.firebaseio.com");
 		var sync = $firebase(ref);
 		$scope.chatMessages = sync.$asArray();
@@ -13,6 +16,9 @@ app.controller('chatCtrl', ['$scope', '$firebase', function($scope, $firebase) {
 			var chatMessage = {
 				name: name,
 				image: image,
+				strDate: strDate,
+				hours: hours,
+				min: min,
 				message: $scope.chatMessage
 			};
 			$scope.chatMessages.$add(chatMessage);
@@ -28,5 +34,6 @@ app.controller('chatCtrl', ['$scope', '$firebase', function($scope, $firebase) {
 		location.reload();
 	}
 }]);
+
 
 
